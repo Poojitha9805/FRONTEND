d1379c7bea43088c60a3590ff7561fe5e722e68d
Poojitha9805 <173158225+Poojitha9805@users.noreply.github.com>
2026-02-24 12:02:51 +0530

    updated

---
+++ JAVASCRIPT/accessing.js
@@ -0,0 +1,11 @@
+function srinu(){
+            alert("helloo everyonee!!!!!!")
+            /*console.log("hello")*/
+        }
+        /*srinu()*/
+
+        function callingElement(){
+            let v=document.getElementById("mahesh").value
+            let z=document.getElementById("email").value
+            console.log(v)
+        }
---
+++ JAVASCRIPT/external.js
@@ -0,0 +1,1 @@
+console.log("Vivek, chalaa vivekavantudhu")
